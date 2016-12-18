0bec052c5562e7486dbdea53c23550b874528c52
Alexander Warren <arocks124@gmail.com>
2016-12-18 12:17:57 -0500

    Added weather api

--- SilverBot.js
+++ SilverBot.js
@@ -2,20 +2,51 @@ var Discord = require("discord.js");
 var bot = new Discord.Client();
 var prefix = "~";
 
+var request = require("request")
+
+var url = "https://jsonplaceholder.typicode.com"
+var authinfo = require("./authinfo.json");
+
+
 bot.on("message", msg => {
 
-	if(msg.author.bot) return;  
-	// console.log(msg.content.substring(3).trim().toLowerCase());
-	//msg.channel.id === "254062866682871808" && 
+    if(msg.author.bot) return;  
+    // console.log(msg.content.substring(3).trim().toLowerCase());
+    //msg.channel.id === "254062866682871808" && 
     if (msg.author.username === "Silver Bot") return;
 
     if(!msg.content.startsWith(prefix)) return;
 
     //msg.content = msg.content.substring(3);//.trim();
+
     mes = msg.content.substring(1).trim().toLowerCase();
+    if(mes.startsWith("weather")){
+        var city = mes.substring(7).trim();
+        request({
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + authinfo.keys.weather,
+            json: true
+        }, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var tempk = body.main.temp;
+                var tempc = Math.max( Math.round((tempk - 273.15) * 10) / 10, 2.8 ).toFixed(2);
+                var tempf = Math.max( Math.round((tempc * 9/5 + 32) * 10) / 10, 2.8 ).toFixed(2);
+
+                tempc * 9/5 + 32;
+
+                msg.channel.sendMessage("Weather in " + city + ":\n" +
+                                        body.weather[0].description + "\n" +
+                                        "temperature: " + tempf + "˚F/" + tempc + "˚C"); // Print the json response
+            }else{
+                msg.channel.sendMessage("Something went wrong. You may have typed the city name wrong or are sending too many requests.");
+                console.log("error: " + error + "\n response.statusCode: " + response.statusCode);
+            }
+        });
+    }
+    
+    console.log("message sent in " + msg.channel.guild.name + "\n\"" + msg.content + "\"\n");
 
     if(mes ===  "fat"){
-    	msg.channel.sendMessage("esdesign");
+        msg.channel.sendMessage("esdesign");
     }
 
     if(mes === "uptime"){
@@ -24,16 +55,23 @@ bot.on("message", msg => {
     if(mes === "rtd"){
         msg.channel.sendMessage(msg.author.username + " rolled a " + Math.floor(Math.random() * (6 - 1 + 1) + 1));
     }
+
+    if(mes === "deletelastmsg"){
+        msg.delete()
+            .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+            .catch(console.error);
+    }
     
     if(mes === "help"){
-    	msg.channel.sendMessage("```SilverBot was created by arocks124#9318\r\
+        msg.channel.sendMessage("```SilverBot was created by arocks124#9318\r\
         to use, type ~ <command>\n\
-    	Commands:\n\
-    	fat\n\
+        Commands:\n\
+        fat\n\
         uptime\n\
         rtd\n\
-    	\n\
-    	Thanks for using SilverBot!```");
+        weather (~weather <city>)\n\
+        \n\
+        Thanks for using SilverBot!```");
     }
 
     if(mes === "git"){
@@ -48,7 +86,7 @@ bot.on("guildMemberAdd", (member) => {
 });
 
 bot.on('ready', () => {
-  console.log('I am ready!');
+    console.log('I am ready!');
 });
 
-bot.login("token");
+bot.login(authinfo.keys.discord);
